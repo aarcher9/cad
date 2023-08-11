f918e0d14b62833686677ac181a8d423a9d20288
aarcher9 <andrea.agosti9@gmail.com>
2023-08-11 17:00:33 +0200

    better code

--- es1.asm
+++ es1.asm
@@ -1,20 +1,34 @@
 .data 0x10008000
 op_code: .byte '+'
 array_1: .word 3 3 2 '!'
+op_result: .byte ''
 
 .text 0x400000
 main:
         # Predispongo parametri e valori di ritorno
-        li $s0, 0 # Somma
-        li $s1, 1 # Prodotto
-        li $s2, 0 # <first> AND <third>
-        la $s3, array_1 # Indirizzo di partenza
-        lb $s4, op_code # Codice operazione
-
+        jal initialize
         jal op
+        jal free
         
         li $v0, 10
         syscall
+
+initialize:
+        li $a0, 0
+        li $s0, 0 # Somma
+        li $s1, 1 # Prodotto
+        li $s2, 1 # <first> AND <third>
+        la $s3, array_1 # Indirizzo di partenza
+        lb $s4, op_code # Codice operazione
+        jr $ra
+
+free:
+        move $s0, $0 # Somma
+        move $s1, $0 # Prodotto
+        move $s2, $0 # <first> AND <third>
+        move $s3, $0 # Indirizzo di partenza
+        move $s4, $0 # Codice operazione
+        jr $ra
 
 op:
         lw $a0, ($s3)
